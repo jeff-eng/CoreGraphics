f98b1ba2b40951e39b712e49db0ce0ddcf8138d3
Jeff Eng <jeff@jeffeng.me>
2016-09-12 16:01:13 -0700

    created initial project and the user interface consisting of image view and button

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Jeffrey Eng on 9/12/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func redrawTapped(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
