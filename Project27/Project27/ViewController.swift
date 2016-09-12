5a1df84464bfcc76051790b77f67bafcf41586c9
Jeff Eng <jeff@jeffeng.me>
2016-09-12 16:17:04 -0700

    set currentDrawType variable and added basic structure for the redrawTapped method

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -9,22 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentDrawType = 0
+    
     @IBOutlet weak var imageView: UIImageView!
 
     @IBAction func redrawTapped(sender: AnyObject) {
         
+        // increment the currentDrawType variable to cycle to the next draw type
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            // reset the draw type back to the beginning of the set
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+            
+        default:
+            break
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+        // calling drawRectangle method here so that something shows up on the screen when app loads
+        drawRectangle()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    func drawRectangle() {
+        
+    }
+    
 }
 
