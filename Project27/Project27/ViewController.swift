d909920af0a60ef9cf463bbc7a64a195c0a6095f
Jeff Eng <jeff@jeffeng.me>
2016-09-13 15:46:40 -0700

    drawRectangle method now creates the rectangle using Core Graphics

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -46,7 +46,30 @@ class ViewController: UIViewController {
     }
 
     func drawRectangle() {
+        // Create context - specify dimensions, opacity, and zero to match scale of screen
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 512, height: 512), false, 0)
+        // Pull out a reference to the current context
+        let context = UIGraphicsGetCurrentContext()
         
+        // Create rectangle
+        let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+        // Set fill color to red (convert UIColor to CGColor)
+        CGContextSetFillColorWithColor(context, UIColor.redColor().CGColor)
+        // Set stroke color to black (convert UIColor to CGColor)
+        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+        // Set line width to 10
+        CGContextSetLineWidth(context, 10)
+        // Adds a CGRect rectangle to the context's current path to be drawn
+        CGContextAddRect(context, rectangle)
+        // Draw the context's current path using the state configured
+        CGContextDrawPath(context, .FillStroke)
+        
+        // Get contents of the drawing as a UIImage
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        // End drawing
+        UIGraphicsEndImageContext()
+        
+        imageView.image = img
     }
     
 }
